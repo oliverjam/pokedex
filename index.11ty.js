5ab5af903169800adc1bf3b7a5860be538b98b93
oliverjam <oliverjamesphillips@gmail.com>
2019-12-04 18:19:27 +0000

    Implement basic pokemon search

--- index.11ty.js
+++ index.11ty.js
@@ -7,9 +7,89 @@ exports.data = {
 exports.render = data => {
   return html`
     <h1>Pokedex</h1>
-    <ol class="grid">
-      ${data.allPokemon.map(Pokemon)}
-    </ol>
+    <section class="stack" aria-label="search pokemon">
+      <form>
+        <label for="search" class="vh">Search</label>
+        <div class="search">
+          <input
+            id="search"
+            name="search"
+            type="search"
+            autocomplete="off"
+            autofocus
+            placeholder="e.g. Charizard"
+          />
+          <button id="clear" type="button" aria-label="clear search">
+            <svg
+              viewBox="0 0 32 32"
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentcolor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              style="transform: scaleX(-1)"
+            >
+              <path
+                d="M29 16 C29 22 24 29 16 29 8 29 3 22 3 16 3 10 8 3 16 3 21 3 25 6 27 9 M20 10 L27 9 28 2"
+              ></path>
+            </svg>
+          </button>
+          <button type="submit" aria-hidden="true" tabindex="-1">
+            <svg
+              viewBox="0 0 32 32"
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentcolor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              aria-hidden="true"
+            >
+              <circle cx="14" cy="14" r="12"></circle>
+              <path d="M23 23 L30 30"></path>
+            </svg>
+          </button>
+        </div>
+      </form>
+    </section>
+    <section aria-label="pokemon">
+      <ol class="grid">
+        ${data.allPokemon.map(Pokemon)}
+      </ol>
+    </section>
+    <script>
+      const form = document.querySelector("form");
+      form.addEventListener("submit", event => {
+        event.preventDefault();
+        const name = event.target.elements.search.value;
+        showOnlyMatches(name);
+      });
+
+      document.querySelector("#clear").addEventListener("click", () => {
+        showOnlyMatches("");
+        form.reset();
+      });
+
+      const searchInput = document.querySelector("#search");
+      const allPokemon = document.querySelectorAll(".poke-link");
+      function showOnlyMatches(value) {
+        let first;
+        allPokemon.forEach(pokemon => {
+          const name = pokemon.querySelector("h2").textContent;
+          const match = name.includes(value);
+          pokemon.hidden = !match;
+          if (!first && match) first = pokemon;
+        });
+        if (value && first) {
+          first.querySelector("a").focus();
+        } else {
+          searchInput.focus();
+        }
+      }
+    </script>
   `;
 };
 
